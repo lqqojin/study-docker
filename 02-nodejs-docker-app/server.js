4ccdc27e327f953c5f5b16d990e1c10c9784742d
lqqojin <lqqojin@gmail.com>
2020-09-04 07:46:46 +0900

    [init]

---
+++ 02-nodejs-docker-app/server.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const PORT = 8080;
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on ${PORT}`)
+})
